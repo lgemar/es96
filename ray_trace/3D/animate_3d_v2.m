84c02fdf74f2fca13aa980e051b7b36d9ea665e3
cstowell <cstowell@college.harvard.edu>
2015-03-24 20:06:16 -0400

    visualization of mirror_spot_pattern and detector_spot_pattern complete

--- ray_trace/3D/animate_3d_v2.m
+++ ray_trace/3D/animate_3d_v2.m
@@ -53,63 +53,78 @@ lens_r2 = 300;
 lens_ctr1 = [9 0 0]' + lens_r1 * [1 0 0]'; 
 lens_ctr2 = [9.5 0 0]' + lens_r2 * [1 0 0]'; 
 
-mirror_spot_pattern = figure(2); 
-hold on; 
-title('Mirror spot pattern')
-
-detector_spot_pattern = figure(3); 
-hold on; 
-title('Deetector spot pattern')
-
 cavity_pulses = {P_init};
 harriet_pulses = {}; 
+N = 1000; % number of frame updates
+
+% preallocate matrices for mirror and detector spot patterns
+numbruns = N*length(cavity_pulses);
+mirror_spots = zeros(numbruns, 2);
+detector_spots = zeros(numbruns, 2);
+
+%color gradient
+c = linspace(1,10,numbruns);
+
 % 1) Think about laser overlap
 % 2) Plotting points on mirror in 2D 
 % Outer loop is the number of frame updates
 % Inner loop updates each individual pulse
-for i = 1:1000   
+for i = 1:N   
     % Take a care of all of the cavity pulses
     for j = 1:length(cavity_pulses)  
+        % index of run
+        index = i*j;
+        
         % Grab the current pulse that will travel through the system
         P = cavity_pulses{j};
         
         % Extend the pulse to the second lens and create bleedthrough
         [P, P2] = P.spherical_mirror_constraint(ctr1, r, dt); 
         figure(model_3d)
-        P.draw(); 
+%        P.draw(); 
         
-        % Switch to spot pattern plot
-        figure(mirror_spot_pattern)
-        plot(P2.p(2), P2.p(3), 'ro')
-        
-        % Switch back to 3d model
-        figure(model_3d)
+        % Record mirror spot pattern
+        mirror_spots(index,1) = P2.p(2);
+        mirror_spots(index,2) = P2.p(3);
         
         % Extend the pulse back to the first lens and create bleedthrough
         [P2, P3] = P2.spherical_mirror_constraint(ctr2, r, dt);
-        P2.draw(); 
+%        P2.draw(); 
         
         % Intersect the ray with the first surface of the lens
         P = P.lens_constraint(lens_ctr1, lens_r1, 1, 5, dt); 
-        P.draw(); 
+%        P.draw(); 
         
         % Intersect the ray with the second surface of the lens
         P = P.lens_constraint(lens_ctr2, lens_r2, 5, 1, dt); 
-        P.draw(); 
+%        P.draw(); 
         
         % Intersect the ray with the plane of the detector
         P = P.vertical_plane_constraint(l1 + 4);
-        P.draw(); 
-        
-        % Switch to detector spot pattern plot
-        figure(detector_spot_pattern)
-        plot(P.p(2), P.p(3), 'ro')
-        
-        % Switch back to 3d model
-        figure(model_3d)
+%        P.draw(); 
+
+        % Record detector spot pattern
+        detector_spots(index,1) = P.p(2);
+        detector_spots(index,2) = P.p(3);
         
         % Update the cavity pulse ray
         cavity_pulses = {P3};
     end
     drawnow;
 end
+
+mirror_spot_pattern = figure(2); 
+hold on; 
+title('Mirror spot pattern')
+
+detector_spot_pattern = figure(3); 
+hold on; 
+title('Detector spot pattern')
+
+% plot mirror spot pattern 
+figure(mirror_spot_pattern)
+scatter(mirror_spots(:,1), mirror_spots(:,2),[], c, '.')
+
+% plot detector spot pattern
+figure(detector_spot_pattern)
+scatter(detector_spots(:,1), detector_spots(:,2),[], c, '.')
