0aa08d07a295aea0524604d3f88eba3f7d042e99
Lukas Gemar <lukasgemar@college.harvard.edu>
2015-04-21 15:54:38 -0400

    ADded parameter sweep function

---
+++ ray_trace/3D/icos_parameter_sweep.m
@@ -0,0 +1,13 @@
+%% Specify interface for ICOS fns
+% param[in] @a n_lens is the number of lenses
+% param[in] @a d_lens is the diameter of the lens, an n_lensx1 vector
+% param[in] @a f_lens is the focal length of the lens, an n_lensx1 vector
+% param[in] @a w_lens is the center thickness of the lenses, a n_lensx1
+%   vector
+% param[in] @a R1 is the concave radius of curvature for the lens, a
+%   n_lensx1 vector
+% param[in] @a R2 is the convex radius of curvature for the lens
+%   n_lensx1 vector
+% param[in] @a gap_lens is the distance from the back of the ICOS cell
+% param[in] @a r_icos is the radius of curvature for ICOS mirrors
+% param[in] @a l_cavity is the length of the ICOS cavity
