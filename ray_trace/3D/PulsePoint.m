78ccdaa46b679046c6c9e29bcff963cdfe5a8a17
cstowell <cstowell@college.harvard.edu>
2015-04-22 01:25:52 -0400

    implemented try catch for fzero and other errors. only draws if P exists. added some real ISP values

--- ray_trace/3D/PulsePoint.m
+++ ray_trace/3D/PulsePoint.m
@@ -14,10 +14,14 @@ classdef PulsePoint
         end
         
         function P = draw(P)
-            x = [P.pos_prev(1) P.p(1)];
-            y = [P.pos_prev(2) P.p(2)];
-            z = [P.pos_prev(3) P.p(3)];
-            h = plot3(x, y, z, 'r');
+            if isempty(P)
+                return
+            else
+                x = [P.pos_prev(1) P.p(1)];
+                y = [P.pos_prev(2) P.p(2)];
+                z = [P.pos_prev(3) P.p(3)];
+                h = plot3(x, y, z, 'r');
+            end
         end
         
         function [P] = vertical_plane_constraint(P, x_pos, n1, n2)
@@ -33,12 +37,16 @@ classdef PulsePoint
         function P = lens_constraint(P, ctr, r, n1, n2) 
              % point of intersection on lens
              dtoarc = @(d) sum((P.p + d*P.dir - ctr).^2) - r^2;
-             d = fzero(dtoarc, 0);
-             % Update Pulse variables for refraction
-             P.pos_prev = P.p; 
-             P.p = P.p + d*P.dir; 
-             % find new direction
-             P.dir = refract_ray(P.pos_prev, P.p, ctr - P.p, n1, n2);
+             try
+                 d = fzero(dtoarc, 0);
+                 % Update Pulse variables for refraction
+                 P.pos_prev = P.p; 
+                 P.p = P.p + d*P.dir; 
+                 % find new direction
+                 P.dir = refract_ray(P.pos_prev, P.p, ctr - P.p, n1, n2);
+             catch
+                 P = [];
+             end    
         end
         
         function [P, P2] = spherical_mirror_constraint(P, ctr, R, n1, n2)
--- ray_trace/3D/animate_3d_v2.m
+++ ray_trace/3D/animate_3d_v2.m
@@ -14,7 +14,7 @@ hold on
     % the RIM
     R_RIM = cm2in(39.37); % radius of curvature of RIM (in)
     distance_RIM = 9.84; % distance between RIM mirror and 1st ICOS mirror
-    reflect_RIM = 1;
+    reflect_RIM = .98;
     % the ICOS mirrors
     r = 1.5; % mirror radius (in)
     R = 30; % radius of curvature of the ICOS mirrors (in)
@@ -24,20 +24,44 @@ hold on
     ctr_thick = cm2in(0.2); % center thickness (cm)
     
     % the lenses
-    l1 = l+1.4; % position of first lens, inch past second ICOS mirror
-    lf = l1 + 2*r; % d/f = 1
-    R_CX = cm2in(25); % Lens radii (1) 
-    R_CC = cm2in(50); % Lens radii (2)
-    ct = .2; % center thickness of lens
-    ct2 = .2;
-    l2 = l1 + 4; % position of second lens
-    l3 = l2 + 2; % position of third lens
+    r1 = 1.5; %radius of first lens
+    R_CX_1 = cm2in(8.0122); % Lens radii (1) 
+    R_CC_1 = cm2in(29.8275); % Lens radii (2)
+    ct1 = cm2in(.9); % center thickness of lens
+    fl1 = cm2in(7.62); % focal length of first lens
+    l1 = l + 1; % position of first lens, inch past second ICOS mirror
+    
+    r2 = .5; %radius of second lens
+    R_CX_2 = cm2in(5.8489); % Lens radii (1) 
+    R_CC_2 = cm2in(2.123); % Lens radii (2)
+    ct2 = cm2in(.3); % center thickness of lens
+    fl2 = 1; % focal length of second lens
+    l2 = l + fl1; % position of second lens
+    
+    r3 = .5; %radius of third lens
+    R_CX_3 = cm2in(8.0122); % Lens radii (1) 
+    R_CC_3 = cm2in(29.8275); % Lens radii (2)
+    ct3 = cm2in(.9); % center thickness of lens
+    fl3 = cm2in(7.62); % focal length of third lens
+    l3 = l2 + fl2; % position of third lens
+    
+    r4 = .5; %radius of fourth lens
+    R_CX_4 = cm2in(8.0122); % Lens radii (1) 
+    R_CC_4 = cm2in(29.8275); % Lens radii (2)
+    ct4 = cm2in(.9); % center thickness of lens
+    fl4 = cm2in(7.62); % focal length of fourth lens
+    l4 = l3 + fl3; % position of fourth lens
     
     second = true; % second lens implemented
-    third = false; % third lense implemented?
+    third = true; % third lense implemented?
+    fourth = false; % fourth lense implemented?
     
     %the detector
-    l4 = l2 + 1; % position of detector
+    if fourth
+        ld = l4 + 1; % position of detector
+    else
+        ld = l3 + 1; % position of detector
+    end
     size = cm2in(1); % size of detector
 
 % make and draw two mirrors in cavity
@@ -46,22 +70,22 @@ mirror3d(l, 0, -1, w, r, R);
 mirror3d(-distance_RIM, 0, -1, w, r, R); 
 
 % Draw the lense after the cavity
-lens3d( l1, r, R_CX, R_CC, ct);
+lens3d( l1, r1, R_CX_1, R_CC_1, ct1);
 if second
-    lens3d( l2, r/2, R_CX, R_CC, ct2);
+    lens3d( l2, r2, R_CX_2, R_CC_2, ct2);
 end
 if third
-    lens3d( l3, r, R_CX, R_CC, ct3);
+    lens3d( l3, r3, R_CX_3, R_CC_3, ct3);
+end
+if fourth
+    lens3d( l4, r4, R_CX_4, R_CC_4, ct4);
 end
 
 % Draw the cube 
-cube3d([l4, -(size/2), -(size/2)], size);
-  
-% Draw the lens
-L = lens(l1, r, R_CX, R_CC, ct);
+cube3d([ld, -(size/2), -(size/2)], size);
 
 % Make it look pretty
-set(gca,'xlim', [-(distance_RIM+1) l4+1], 'ylim', 10*[-r r], 'DataAspectRatio',[1 1 1],'visible','off');
+set(gca,'xlim', [-(distance_RIM+1) ld+1], 'ylim', 10*[-r r], 'DataAspectRatio',[1 1 1],'visible','off');
 set(gca,'visible','off');
 set(gcf,'color',[.75 .75 1]);
 alpha(.5);
@@ -83,13 +107,16 @@ mirror2 = mirror(l,r,R,-1,reflect, ctr_thick);
 RIMirror = mirror(-distance_RIM,r,R_RIM,1,reflect_RIM, ctr_thick);
 
 % create lenses
-lens1 = lens(l1, r, R_CX, R_CC, ct);
+lens1 = lens(l1, r1, R_CX_1, R_CC_1, ct1);
 % TODO change parameters
 if second
-    lens2 = lens(l2, r, R_CX, R_CC, ct);
+    lens2 = lens(l2, r2, R_CX_2, R_CC_2, ct2);
 end
 if third
-    lens3 = lens(l3, r, R_CX, R_CC, ct);
+    lens3 = lens(l3, r3, R_CX_3, R_CC_3, ct3);
+end
+if fourth
+    lens3 = lens(l4, r4, R_CX_4, R_CC_4, ct4);
 end
 
 N1 = 3; % number of RIM reflections
@@ -126,6 +153,19 @@ for i = 1:N1
     
     for j = 1:N2
         
+        if j > 1
+            
+            % ******* FOLLOWING P_left in next inner loop ******** 
+
+            % Extend the pulse to the first mirror and reflect back as P_cavity 
+            % for next loop
+            [P_left, P_cavity] = P_left.spherical_mirror_constraint(mirror1.ctr, mirror1.R, n_air, n_ZnSe); 
+            P_left.draw();
+
+            % Draw the pulses up until now
+            drawnow;  
+        end
+        
         P_rt = P_cavity;
 
         % Extend the pulse back to the second mirror and create bleedthrough
@@ -140,47 +180,78 @@ for i = 1:N1
 
         % Intersect the ray with the first surface of the first lens
         P = P_rt.lens_constraint(lens1.ctr1, lens1.R_CX, n_air, n_ZnSe); 
-        P.draw(); 
+        if isempty(P)
+            continue
+        else
+            P.draw(); 
+        end
 
         % Intersect the ray with the second surface of the first lens
         P = P.lens_constraint(lens1.ctr2, lens1.R_CC, n_ZnSe, n_air); 
-        P.draw(); 
+        if isempty(P)
+            continue
+        else
+            P.draw();
+        end 
 
         if second
             % Intersect the ray with the first surface of the second lens
             P = P.lens_constraint(lens2.ctr1, lens2.R_CX, n_air, n_ZnSe); 
-            P.draw(); 
+            if isempty(P)
+                continue
+            else
+                P.draw();
+            end 
             % Intersect the ray with the second surface of the second lens
             P = P.lens_constraint(lens2.ctr2, lens2.R_CC, n_ZnSe, n_air); 
-            P.draw();     
+            if isempty(P)
+                continue
+            else
+                P.draw();
+            end     
         end
         if third
             % Intersect the ray with the first surface of the third lens
             P = P.lens_constraint(lens3.ctr1, lens3.R_CX, n_air, n_ZnSe); 
-            P.draw(); 
+            if isempty(P)
+                continue
+            else
+                P.draw();
+            end
             % Intersect the ray with the second surface of the third lens
             P = P.lens_constraint(lens3.ctr2, lens3.R_CC, n_ZnSe, n_air); 
-            P.draw();  
-        end    
+            if isempty(P)
+                continue
+            else
+                P.draw();
+            end
+        end 
+        if fourth
+            % Intersect the ray with the first surface of the third lens
+            P = P.lens_constraint(lens4.ctr1, lens4.R_CX, n_air, n_ZnSe); 
+            if isempty(P)
+                continue
+            else
+                P.draw();
+            end 
+            % Intersect the ray with the second surface of the third lens
+            P = P.lens_constraint(lens4.ctr2, lens4.R_CC, n_ZnSe, n_air); 
+            if isempty(P)
+                continue
+            else
+                P.draw();
+            end  
+        end 
 
         % Intersect the ray with the plane of the detector
-        [P] = P.vertical_plane_constraint(l4, 1, 1);
+        [P] = P.vertical_plane_constraint(ld, 1, 1);
         P.draw();
-        
+
         % Determine if within angle of +/- 15 degrees
-         angle = radtodeg(acos(dot(P.dir,[1;0;0])));
-         if abs(P.p(2))<cm2in(10) && abs(P.p(3))<cm2in(10)
-             detect_pow = detect_pow + P.pow;
-         end
+        angle = radtodeg(acos(dot(P.dir,[1;0;0])));
+        if abs(P.p(2))<cm2in(10) && abs(P.p(3))<cm2in(10)
+            detect_pow = detect_pow + P.pow;
+        end
 
-        % ******* FOLLOWING P_left in next inner loop ******** 
-
-        % Extend the pulse to the first mirror and reflect back as P_cavity 
-        % for next loop
-        [P_left, P_cavity] = P_left.spherical_mirror_constraint(mirror1.ctr, mirror1.R, n_air, n_ZnSe); 
-        P_left.draw();
-        
-        % Draw the pulses up until now
-        drawnow;  
     end
 end
